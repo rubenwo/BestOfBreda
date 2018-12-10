8b4441ef0a6d45d78c43fe515ce0911bc7216c76
RickWagenmakers <rpwwagen@avans.nl>
2018-12-10 14:57:27 +0100

    Auto stash before merge of "viewmodels" and "origin/viewmodels"

---
+++ app/src/main/java/com/a6/projectgroep/bestofbreda/Services/VolleyConnection.java
@@ -0,0 +1,104 @@
+package com.a6.projectgroep.bestofbreda.Services;
+
+import android.content.Context;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Random;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.maps.model.LatLng;
+
+public class VolleyConnection {
+
+    RequestQueue queue;
+    public static VolleyConnection volleyConnection;
+
+    private VolleyConnection(Context context)
+    {
+        queue = Volley.newRequestQueue(context);
+    }
+
+    public static VolleyConnection getInstance(Context context)
+    {
+        if(volleyConnection == null)
+        {
+            volleyConnection = new VolleyConnection(context);
+        }
+        return volleyConnection;
+    }
+
+    public String getRouteURL(LatLng origin, LatLng dest)
+    {
+        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;       // Detination of route
+        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
+        String trafficMode = "mode=walking";
+        // trafficMode = "mode=driving";
+        // Building the parameters to the web service
+        String parameters = str_origin + "&" + str_dest + "&" + trafficMode;
+        // Output format
+        String output = "json";
+        // Building the url to the web service
+        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters;
+        return url + "&key=AIzaSyAVFvWsRZacQWdZgiRfd8A3T04zfwvmYBE";
+    }
+
+    public ArrayList<LatLng> getRoute(LatLng origin, LatLng dest)
+    {
+        String url = getRouteURL(origin, dest);
+        ArrayList<LatLng> routePoints = new ArrayList<>();
+        final JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+
+                new Response.Listener<JSONObject>(){
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.i("blindwall", "OK");
+                        try {
+                            System.out.println(response);
+                            for( int idx = 0; idx < response.getJSONArray("routes").getJSONArray(0).length(); idx++) {
+
+//                                String author = response.getJSONObject(idx).getString("author");
+//                                String desc = response.getJSONObject(idx).getJSONObject("description").getString("nl");
+//                                String desc2 = response.getJSONObject(idx).getJSONObject("description").getString("en");
+//                                JSONArray images = response.getJSONObject(idx).getJSONArray("images");
+//                                String material = response.getJSONObject(idx).getJSONObject("material").getString("nl");
+//                                String adress = response.getJSONObject(idx).getString("address");
+//                                String fotograaf = response.getJSONObject(idx).getString("photographer");
+//                                int index = new Random().nextInt(images.length());
+//
+//                                String imageUrl = "https://api.blindwalls.gallery/" +
+//                                        images.getJSONObject(index).getString("url");
+
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+
+                new Response.ErrorListener(){
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.i("blindwall", "NOK");
+                    }
+                }
+        );
+
+        return routePoints;
+    }
+}
--- app/src/main/java/com/a6/projectgroep/bestofbreda/ViewModel/MainViewModel.java
+++ app/src/main/java/com/a6/projectgroep/bestofbreda/ViewModel/MainViewModel.java
@@ -7,16 +7,19 @@ import android.support.annotation.NonNull;
 
 import com.a6.projectgroep.bestofbreda.Model.WayPointModel;
 import com.a6.projectgroep.bestofbreda.Services.GoogleMapsAPIManager;
+import com.a6.projectgroep.bestofbreda.Services.VolleyConnection;
 import com.google.android.gms.maps.model.LatLng;
 
 import java.util.ArrayList;
 
 public class MainViewModel extends AndroidViewModel {
     public GoogleMapsAPIManager mapsApiManager;
+    public VolleyConnection volleyConnection;
 
     public MainViewModel(@NonNull Application application) {
         super(application);
         mapsApiManager = GoogleMapsAPIManager.getInstance(application);
+        volleyConnection = VolleyConnection.getInstance(application.getApplicationContext());
     }
 
     public LatLng getCurrentPosition()
@@ -27,4 +30,10 @@ public class MainViewModel extends AndroidViewModel {
     public ArrayList<WayPointModel> getAllWaypoints(){
         return mapsApiManager.getRouteWaypoints(getApplication());
     }
+
+    public ArrayList<LatLng> getRoute(LatLng origin, LatLng dest)
+    {
+        return volleyConnection.getRoute(origin, dest);
+    }
+
 }
