d55a15ad472e330e29e8519556b213bd608b7167
Unknown <amchelle@avans.nl>
2018-12-11 09:18:17 +0100

    [Added] Push notification service

---
+++ app/src/main/java/com/a6/projectgroep/bestofbreda/Services/PushNotification.java
@@ -0,0 +1,41 @@
+package com.a6.projectgroep.bestofbreda.Services;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.view.View;
+
+import com.a6.projectgroep.bestofbreda.R;
+
+import static com.a6.projectgroep.bestofbreda.BestOfBreda.CHANNEL_SIGHT_PASSED_ID;
+
+public class PushNotification {
+    private NotificationManagerCompat managerCompat;
+    private static PushNotification instance;
+
+    private PushNotification(Context context)
+    {
+        managerCompat = NotificationManagerCompat.from(context);
+    }
+
+    // currently used Singleton, but can be changed
+    public static PushNotification getInstance(Context context)
+    {
+        if (instance == null)
+            instance = new PushNotification(context);
+        return instance;
+    }
+
+    public void SendSightNotification(View v, String sightName, String sightDescription, Context context) {
+        Notification notification = new NotificationCompat.Builder(context, CHANNEL_SIGHT_PASSED_ID)
+                .setSmallIcon(R.drawable.ic_nav_sights)
+                .setContentTitle(sightName)
+                .setContentText(sightDescription)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .build();
+
+        managerCompat.notify(1, notification);
+    }
+}
