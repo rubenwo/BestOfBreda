68a84604a444b393f15cfbf6830a06a0d92a9b5b
Unknown <maartp@hotmail.com>
2018-12-17 14:45:21 +0100

    [Added] Opens detailedactivity after pressing pushnotification

--- app/src/main/java/com/a6/projectgroep/bestofbreda/Services/PushNotification.java
+++ app/src/main/java/com/a6/projectgroep/bestofbreda/Services/PushNotification.java
@@ -1,13 +1,16 @@
 package com.a6.projectgroep.bestofbreda.Services;
 
 import android.app.Notification;
-import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.view.View;
 
 import com.a6.projectgroep.bestofbreda.R;
+import com.a6.projectgroep.bestofbreda.View.Activities.DetailedActivity;
 
 import static com.a6.projectgroep.bestofbreda.BestOfBreda.CHANNEL_SIGHT_PASSED_ID;
 
@@ -40,11 +43,20 @@ public class PushNotification {
     }
 
     public void SendSightNotification(String sightName, String sightDescription, Context context) {
+        Intent resultIntent = new Intent(context, DetailedActivity.class);
+        resultIntent.putExtra("NotificationMessage", sightName);
+        resultIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        stackBuilder.addNextIntentWithParentStack(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
         Notification notification = new NotificationCompat.Builder(context, CHANNEL_SIGHT_PASSED_ID)
                 .setSmallIcon(R.drawable.ic_nav_sights)
                 .setContentTitle(sightName)
                 .setContentText(sightDescription)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(resultPendingIntent)
                 .build();
 
         managerCompat.notify(1, notification);
