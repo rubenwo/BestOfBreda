235225aadd966f09473db0590ed8dae07d459ab5
RickWagenmakers <rpwwagen@avans.nl>
2018-12-17 16:17:04 +0100

    [added terms of  service

--- app/src/main/java/com/a6/projectgroep/bestofbreda/View/Activities/MainActivity.java
+++ app/src/main/java/com/a6/projectgroep/bestofbreda/View/Activities/MainActivity.java
@@ -11,6 +11,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
@@ -35,6 +36,7 @@ import com.a6.projectgroep.bestofbreda.Services.GeoCoderService;
 import com.a6.projectgroep.bestofbreda.Services.LiveLocationListener;
 import com.a6.projectgroep.bestofbreda.Services.RouteReceivedListener;
 import com.a6.projectgroep.bestofbreda.View.Fragments.DetailedRouteFragment;
+import com.a6.projectgroep.bestofbreda.View.Fragments.TermsOfServiceFragment;
 import com.a6.projectgroep.bestofbreda.ViewModel.MainViewModel;
 import com.a6.projectgroep.bestofbreda.ViewModel.ViewModelFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -132,6 +134,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
                         break;
                     case R.id.menu_nav_termsofservice:
+                        DialogFragment fragment = new TermsOfServiceFragment();
+                        fragment.show(getSupportFragmentManager(), "TOS");
+
 
                         break;
                 }
--- app/src/main/java/com/a6/projectgroep/bestofbreda/View/Fragments/TermsOfServiceFragment.java
+++ app/src/main/java/com/a6/projectgroep/bestofbreda/View/Fragments/TermsOfServiceFragment.java
@@ -1,6 +1,8 @@
 package com.a6.projectgroep.bestofbreda.View.Fragments;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,6 +11,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.a6.projectgroep.bestofbreda.R;
 
@@ -16,6 +19,8 @@ import com.a6.projectgroep.bestofbreda.R;
  * A simple {@link Fragment} subclass.
  */
 public class TermsOfServiceFragment extends DialogFragment {
+
+    private TextView tosText;
 
     public static TermsOfServiceFragment newInstance() {
         TermsOfServiceFragment fragment = new TermsOfServiceFragment();
@@ -26,7 +31,21 @@ public class TermsOfServiceFragment extends DialogFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_terms_of, container, false);
+        tosText = v.findViewById(R.id.termsofactivity_text);
+        tosText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.vvvhartvannoordholland.nl/media/hartvannoordholland/org/documents/algemene%20voorw_vvv_versie041209_%20engels.pdf"));
+                startActivity(browserIntent);
+            }
+        });
+
         return v;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        getDialog().getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+    }
 }
