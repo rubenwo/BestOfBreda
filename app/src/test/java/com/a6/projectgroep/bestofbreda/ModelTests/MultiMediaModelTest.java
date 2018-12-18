009fac6825d31c4c7583359855edddf82e625a38
Matthijs Droogendijk <mbdrooge@avans.nl>
2018-12-18 14:27:43 +0100

    [Added] Model Unittests with Mockito

--- app/src/test/java/com/a6/projectgroep/bestofbreda/ModelTests/MultiMediaModelTest.java
+++ app/src/test/java/com/a6/projectgroep/bestofbreda/ModelTests/MultiMediaModelTest.java
@@ -4,17 +4,23 @@ import com.a6.projectgroep.bestofbreda.Model.MultimediaModel;
 
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class MultiMediaModelTest {
+    private static final MultimediaModel multiMediaModel = mock(MultimediaModel.class);
+    private static final List<String> pictureUrls = mock(List.class);
     @Test
-    public void multiMediaModelTest() {
-        List<String> urls = Arrays.asList("url_1", "url_2");
-        MultimediaModel model = new MultimediaModel(Arrays.asList("url_1", "url_2"), "videoURL");
-        assertEquals(model.getPictureUrls(), urls);
-        assertEquals("videoURL", model.getVideoUrls());
+    public void createMultiMediaModel_isCorrect(){
+        multiMediaModel.setId(1);
+        verify(multiMediaModel).setId(1);
+        pictureUrls.add("pictureUrl1");
+        pictureUrls.add("pictureUrl2");
+        multiMediaModel.setPictureUrls(pictureUrls);
+        verify(multiMediaModel).setPictureUrls(pictureUrls);
+        multiMediaModel.setVideoUrls("videoUrl");
+        verify(multiMediaModel).setVideoUrls("videoUrl");
     }
 }
--- app/src/test/java/com/a6/projectgroep/bestofbreda/ModelTests/RouteModelTest.java
+++ app/src/test/java/com/a6/projectgroep/bestofbreda/ModelTests/RouteModelTest.java
@@ -1,11 +1,29 @@
 package com.a6.projectgroep.bestofbreda.ModelTests;
 
+import com.a6.projectgroep.bestofbreda.Model.RouteModel;
+
 import org.junit.Test;
 
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 public class RouteModelTest {
+    private static final RouteModel routeModel = mock(RouteModel.class);
+    private static final List<String> route = mock(List.class);
 
     @Test
-    public void routeModelTest() {
-        
+    public void createRouteModel_isCorrect() {
+        routeModel.setName("routeName");
+        verify(routeModel).setName("routeName");
+        route.add("waypoint1");
+        route.add("waypoint2");
+        routeModel.setRoute(route);
+        verify(routeModel).setRoute(route);
+        routeModel.setDone(true);
+        verify(routeModel).setDone(true);
+        routeModel.setResourceID("resourceID");
+        verify(routeModel).setResourceID("resourceID");
     }
 }
